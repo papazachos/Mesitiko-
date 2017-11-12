9869dabc5e05c0f853f573ec947427f5ea8bdfab
papazachos <32549642+papazachos@users.noreply.github.com>
2017-11-12 20:14:27 +0200

    Add files via upload

---
+++ papazachos/commit/Googlemaps_serres_commit.java
@@ -0,0 +1,47 @@
+
+package googlemaps_serres_commit;
+
+/**
+ *
+ * @author Δημητρης
+ */
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng serres = new LatLng(41.090923, 23.54132);
+        mMap.addMarker(new MarkerOptions().position(serres).title("Serres"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(serres, 15.0f));
+
+    }
+
+
+}
