62d186e3daab0685db86ca97064a1188e388c4bc
papazachos <32549642+papazachos@users.noreply.github.com>
2017-11-20 20:40:01 +0200

    Viki 20/11/17
    commits 20/11/17

---
+++ Viki_20_11_17/src/viki_20_11_17/Viki_20_11_17.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viki_20_11_17;
+
+/**
+ *
+ * @author Δημητρης
+ */
+public class Viki_20_11_17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
